2efd04c13d720c9ce4c923ce997171a9cfce4419
suresh-TR <suresh.fedev@gmail.com>
2026-09-15 21:30:22 +0530

    changes to header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
 
 console.log(mount)
 
 export default () => {
     return (<div>
-        <h1>Hi there</h1><hr /><MarketingApp /></div>);
+        <BrowserRouter>
+            <Header />
+            <MarketingApp />
+        </BrowserRouter>
+    </div>);
 }
